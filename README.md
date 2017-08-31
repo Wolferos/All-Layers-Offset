65d2f83593c67f82332822fd9d1cd16b50c03736
Geoffrey <wolferos@laposte.net>
2017-08-31 11:34:30 +0200

    Upload #1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-AllOffsetLayers
+# All Layers Offset
+All Layers Offset is a GIMP Python plug-in.
+It's forked from **Group Offset** by _Vesa Kivimäki_ (http://registry.gimp.org/node/26996)
+This script allows you to apply an offset to all your layers.
+
+# Installation
+If you don't know how to install a GIMP plug-in, see https://en.wikibooks.org/wiki/GIMP/Installing_Plugins
+
+Once installed, you can execute the script via **GIMP -> Group -> All Layers Offset...**
---
+++ plug-ins/group_offset_all.py
@@ -0,0 +1,60 @@
+﻿#!/usr/bin/env python
+
+###################################################
+##### All Layers Offset - GIMP plugin
+##### (c) Wolferos Productions 2017
+##### Forked from Group Offset by Vesa Kivimäki (http://registry.gimp.org/node/26996)
+##### Released under GNU General Public License v2
+###################################################
+
+from gimpfu import *
+import gtk
+
+def debugMessage(Message):
+    dialog = gtk.MessageDialog(None, 0, gtk.MESSAGE_INFO, gtk.BUTTONS_OK, Message)
+    dialog.run()
+    dialog.hide()
+
+def py_group_offset_all(img, tdraw, xoffset, yoffset, edge, half):
+
+        def doAction():
+              ll = img.layers
+              for l in ll:
+                 if (edge == "wrap"):
+                   pdb.gimp_drawable_offset(l, True, 0, xoffset, yoffset)
+                 if (edge == "bg"):
+                   pdb.gimp_drawable_offset(l, False, 0, xoffset, yoffset)
+                 if (edge == "trans"):
+                   pdb.gimp_drawable_offset(l, False, 1, xoffset, yoffset)
+
+        ### if half is set, set offsets to 1/2 image size
+        if half:
+           xoffset = (img.width // 2)
+           yoffset = (img.height // 2)
+
+        pdb.gimp_image_undo_group_start(img)
+        doAction()
+        pdb.gimp_image_undo_group_end(img)
+
+register(
+        "py_group_offset_all",
+        "All Layers Offset",
+        "Apply Offset to all Layers",
+        "(c) Wolferos Productions",
+        "GNU General Public License v2",
+        "2012-2017",
+        "<Image>/Group/All Layers Offset...", 
+        "RGB*, GRAY*",
+        [
+                (PF_SPINNER, "xoffset", "X Offset", 0, (-32768, 32767, 1)),
+                (PF_SPINNER, "yoffset", "Y Offset", 0, (-32768, 32767, 1)),
+                (PF_RADIO, "edge", "Edge behaviour", "wrap", (
+                     ("Wrap around", "wrap"),
+                     ("Fill with bg", "bg"),
+                     ("Transparent", "trans"))),
+                (PF_TOGGLE, "half", "Offset by 1/2 of image", False)
+        ],
+        [],
+        py_group_offset_all)
+
+main()
